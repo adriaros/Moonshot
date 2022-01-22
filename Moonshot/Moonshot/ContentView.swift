f47faa945a0d283f35a9c0bec81580b0bd1ffb30
AdriaRos <adria.ros@elma.care>
2022-01-22 19:41:53 +0100

    main
    - Added project

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Adrià Ros on 22/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
