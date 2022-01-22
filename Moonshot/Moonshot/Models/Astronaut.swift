c49779eda99e2ddcc566bd20815720060d6c68b3
AdriaRos <adria.ros@elma.care>
2022-01-22 20:43:35 +0100

    main
    - Added images and json data
    - Decode the data from json

---
+++ Moonshot/Moonshot/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Adrià Ros on 22/1/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Models/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Adrià Ros on 22/1/22.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
